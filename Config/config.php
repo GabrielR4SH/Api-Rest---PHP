15538e3361f2dd51f008357b8254b3d92c9cdb5c
GabrielR4SH <bieelsantana23@hotmail.com>
2025-01-07 18:06:38 -0300

    first commit

---
+++ Config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+declare(strict_types=1);
+
+define("ROOT_PATH", __DIR__ . "/../");
+define("DATABASE_FILE", ROOT_PATH . 'database.json');
+
+require_once ROOT_PATH . "Controller/Api/BaseController.php";
+
