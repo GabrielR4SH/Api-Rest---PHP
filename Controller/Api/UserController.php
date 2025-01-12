637625d07be90e950bb49ac3a7475d953b7c6d27
GabrielR4SH <bieelsantana23@hotmail.com>
2025-01-11 21:18:09 -0300

    feat: RESTFull incremetend

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -2,6 +2,9 @@
 
 class UserController extends BaseController
 {
+
+    //TODO: Service Page
+    
     public function listAction(){
         
         $erroDescription = '';
@@ -38,5 +41,88 @@ class UserController extends BaseController
 
     }
 
+    public function getUserAction($id){
+        $erroDescription = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
 
+        if(strtoupper($requestMethod) == 'GET'){
+            try{
+                $userModel = new UserModel();
+                $user = $userModel->getUser($id);
+                $responseData = json_encode($user);
+            } catch(Error $e){
+                $errorDescription = $e->getMessage().'Something went wrong! Please contact support';
+                $errorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $errorDescription = "Method not supported";
+            $errorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        //send output
+        if (!$erroDescription){
+            $this->sendOutput($responseData, array('Content-Type: application/json', 'HTTP/1.1 200 Ok'));
+        } else {
+            $this->sendOutput(json_encode(array('error' => $errorDescription)),
+            array('Content-Type: application/json', $errorHeader));
+        }
+    }
+
+        
+    public function deleteUserAction($id){
+        $erroDescription = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+        if(strtoupper($requestMethod) == 'DELETE'){
+            try{
+                $userModel = new UserModel();
+                $userModel->deleteUser($id);
+                $responseData = json_encode(['message' => 'User deleted successfully']);
+            } catch(Error $e){
+                $errorDescription = $e->getMessage().'Something went wrong! Please contact support';
+                $errorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $errorDescription = "Method not supported";
+            $errorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        //send output
+        if (!$erroDescription){
+            $this->sendOutput($responseData, array('Content-Type: application/json', 'HTTP/1.1 200 Ok'));
+        } else {
+            $this->sendOutput(json_encode(array('error' => $errorDescription)),
+            array('Content-Type: application/json', $errorHeader));
+        }
+    }
+
+    
+    public function generateUsersAction(){
+        $erroDescription = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $stringParamsArray = $this->getStringParams();
+
+        if(strtoupper($requestMethod) == 'POST'){
+            try{
+                $userModel = new UserModel();
+                $quantity = $stringParamsArray['quantity'] ?? 10;
+                $userModel->generateUsers($quantity);
+                $responseData = json_encode(['message' => 'Users generated successfully']);
+            } catch(Error $e){
+                $errorDescription = $e->getMessage().'Something went wrong! Please contact support';
+                $errorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $errorDescription = "Method not supported";
+            $errorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        //send output
+        if (!$erroDescription){
+            $this->sendOutput($responseData, array('Content-Type: application/json', 'HTTP/1.1 200 Ok'));
+        } else {
+            $this->sendOutput(json_encode(array('error' => $errorDescription)),
+            array('Content-Type: application/json', $errorHeader));
+        }
+    }
 }
--- Model/UserModel.php
+++ Model/UserModel.php
@@ -1,10 +1,76 @@
 <?php
- require_once ROOT_PATH . "/Model/Database.php";
+require_once ROOT_PATH . "/Model/Database.php";
 
 class UserModel extends Database
 {
     public function getUsers($limit)
     {  
         return $this->select($limit);
+    }
+
+    public function getUser($id)
+    {
+        try {
+            $users = json_decode(file_get_contents(DATABASE_FILE), true);
+            foreach ($users as $user) {
+                if ($user['user_id'] == $id) {
+                    return $user;
+                }
+            }
+            throw new Exception("User not found");
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+        return false;
+    }
+
+    public function updateUser($id, $data)
+    {
+        try {
+            $users = json_decode(file_get_contents(DATABASE_FILE), true);
+            foreach ($users as &$user) {
+                if ($user['user_id'] == $id) {
+                    $user = array_merge($user, $data);
+                    file_put_contents(DATABASE_FILE, json_encode($users));
+                    return true;
+                }
+            }
+            throw new Exception("User not found");
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+        return false;
+    }
+
+    public function deleteUser($id)
+    {
+        try {
+            $users = json_decode(file_get_contents(DATABASE_FILE), true);
+            foreach ($users as $index => $user) {
+                if ($user['user_id'] == $id) {
+                    unset($users[$index]);
+                    file_put_contents(DATABASE_FILE, json_encode(array_values($users)));
+                    return true;
+                }
+            }
+            throw new Exception("User not found");
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+        return false;
+    }
+
+    public function generateUsers($quantity)
+    {
+        $users = [];
+        for ($i = 1; $i <= $quantity; $i++) {
+            $users[] = [
+                'user_id' => $i,
+                'username' => 'User' . $i,
+                'user_email' => 'user' . $i . '@example.com',
+                'user_status' => rand(0, 1)
+            ];
+        }
+        file_put_contents(DATABASE_FILE, json_encode($users));
     }
 }
